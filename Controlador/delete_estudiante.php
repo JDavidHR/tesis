1f7bfa2cf4188f3b9e4f7fd8a975bbc742bf6bad
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2021-06-14 18:09:56 -0500

    Update delete_estudiante.php

--- Controlador/delete_estudiante.php
+++ Controlador/delete_estudiante.php
@@ -8,12 +8,12 @@ if( isset($_POST['submit']) && isset($_POST['usuario'])){
     $mysql = new MySQL(); //se declara un nuevo array
     $mysql->conectar();
     //ejecucion de la consulta a la base de datos
-    $sql = $mysql->efectuarConsulta("UPDATE asistencia.estudiante SET estado = 0 WHERE nombres = ".$estudiante."");
+    $sql = $mysql->efectuarConsulta("UPDATE asistencia.estudiante SET estado = 0 WHERE id_estudiante = ".$estudiante."");
     //Se valida si la consulta arrojo algun valor
     if($sql){
         //mensaje de salida (alert) cuanod la consulta es exitosa con su respectiva redireccion de pagina
         echo"<script type=\"text/javascript\">alert('Se elimino correctamente'); window.location='../index_administrador.php';</script>";
-
+        //echo $estudiante;
     }else{
         //mensaje de salida en caso de que la consulta falle
         echo"<script type=\"text/javascript\">alert('Se produjo un error'); window.location='../registro_usuario.php';</script>";
