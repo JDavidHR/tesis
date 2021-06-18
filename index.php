049217a8e96fd621794d66a6f42d4d6bdf814fca
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2021-06-18 13:24:19 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    
+    <title></title>
+    <!-- Custom CSS -->
+    <link href="css/chartist.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.min.css" rel="stylesheet">
+
+    <link href="css/ie7.css" rel="stylesheet">
+
+    <link href="css/materialdesignicons.min.css" rel="stylesheet">
+    <link href="css/weather-icons.min.css" rel="stylesheet">
+ 
+</head>
+
+<body>
+
+                <?php
+                 //funcion donde se llama al menu superior del usuario
+                  include("login.php");
+                  ?>
+
+            <footer class="footer text-center">
+                All Rights Reserved by asistencias COTECNOVA
+            </footer>
+
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="js/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="js/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
+    <!--This page JavaScript -->
+    <!--chartis chart-->
+    <script src="js/chartist.min.js"></script>
+    <script src="js/chartist-plugin-tooltip.min.js"></script>
+    <script src="js/dashboard1.js"></script>
+</body>
+
+</html>
