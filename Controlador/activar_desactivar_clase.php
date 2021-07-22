71846cb77e877db526d287604761997d69e1b4da
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2021-07-22 16:20:05 -0500

    new files asistencia docente cambio de estados

---
+++ Controlador/activar_desactivar_clase.php
@@ -0,0 +1,29 @@
+<?php
+//condicion donde se rectifica que los campos no esten vacios y que esten definidos
+
+
+
+        require_once '../Modelo/MySQL.php';//se llama la pagina mysql.php para hacer la respectiva conexion con la BD
+        //declaracion de las variables donde se almacenan los datos de los respectivos campos llenados del formulario metodo post
+        $ida_docente = $_GET['ida_docente'];
+
+        $mysql = new MySQL;//nuevo mysql
+        $mysql->conectar();//funcion almacenada en mysql.php
+        //consulta de la insercion de datos en la base de datos, donde hace las respectivas consultas
+        //echo "comentarios: " . $comentarios;
+        //echo "id_clase: " . $id_clase;
+        //echo "fechaclase: " . $fechaclase;
+
+
+        $sql = $mysql->efectuarConsulta("UPDATE asistencia.a_docente SET estado = 'Inactiva' WHERE ida_docente = ".$ida_docente."");
+        //condicion donde si la consulta se hace correcto
+        if($sql){
+            echo"<script type=\"text/javascript\">alert('Se cambio el estado Correctamente'); window.location='../clases_vistas.php';</script>";
+        }else{
+            //mensaje de salida en caso de que la consulta falle
+            echo"<script type=\"text/javascript\">alert('Se produjo un error'); window.location='../registro_clase_docente.php';</script>";
+        }
+        
+    
+
+?>
